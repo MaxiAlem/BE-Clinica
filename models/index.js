5e9c78461e7cf291fab57e86c99734a6c6760b7e
MaxiAlem <maerc.maxi@gmail.com>
2025-05-16 16:40:33 -0300

    arreglos varios

--- models/index.js
+++ models/index.js
@@ -27,7 +27,7 @@ Object.values(models).forEach((model) => {
 const syncModels = async () => {
   try {
     // Sincronizamos todos los modelos
-    await sequelize.sync({ alter: true }); // O false si ya está todo bien
+    await sequelize.sync({ alter: false }); // O false si ya está todo bien
 
     console.log('Modelos sincronizados con la base de datos');
   } catch (error) {
